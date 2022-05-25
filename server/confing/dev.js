ed5294c0623c208504e00181a153afcd9a7cad06
amir7896 <amirshahzad07896@gmail.com>
2022-05-25 17:03:57 +0500

    Emaily app in mern stack

---
+++ server/confing/dev.js
@@ -0,0 +1,10 @@
+module.exports = {
+  googleClientID: "Google Client ID",
+  googleClientSecret: "Google Client Secret",
+  DBString: "MONGOD DB Connection String ",
+  CookieKey: "94qoirajfk89q342@##*jkjsd",
+  stripePublishableKey: "Stripe Pulishable key",
+  stripSecretKey: "Stripe Secret Key",
+  sendGridKey: "Send Grid Key",
+  redirectDomain: "http://localhost:3000",
+};
